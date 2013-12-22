9245d9b5663d48b6caecb316197807e70fc9de99
Jeff Yokum <jyokum@gmail.com>
2013-12-22 10:39:53 -0600

    Corrected invalid tests

--- tests/HealthGraph/Tests/Operations/SleepSetTest.php
+++ tests/HealthGraph/Tests/Operations/SleepSetTest.php
@@ -2,32 +2,17 @@
 
 namespace HealthGraph\Tests\Operations;
 
-class BackgroundActivitySetFeedTest extends \Guzzle\Tests\GuzzleTestCase
+/**
+ * @requires function skip_for_now
+ * @TODO build supporting operations
+ */
+class SleepSetTest extends BaseTest
 {
-    const OPERATION = 'GetBackgroundActivitySetFeed';
 
     protected function setUp()
     {
         $this->client = $this->getServiceBuilder()->get('client');
-    }
-
-    public function testCommandIsValid()
-    {
-        $object = $this->client->getCommand(self::OPERATION);
-        $this->assertInstanceOf('\Guzzle\Service\Command\OperationCommand', $object);
-    }
-
-    public function testIteratorIsValid()
-    {
-        $object = $this->client->getIterator(self::OPERATION);
-        $this->assertInstanceOf('\HealthGraph\Common\Iterator\FeedIterator', $object);
-    }
-
-    public function testPrepareGetCommand()
-    {
-        $command = $this->client->getCommand(self::OPERATION);
-        $command->prepare();
-        $this->assertTrue($command->isPrepared());
+        $this->base = 'SleepSet';
     }
 
 }
