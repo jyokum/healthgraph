17f403e8202b2d82d43a9e961bd0526abad35f42
Jeff Yokum <jyokum@gmail.com>
2013-12-30 09:34:09 -0600

    Updating test bootstrap to include optional logging

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,19 @@ error_reporting(E_ALL | E_STRICT);
 require_once 'PHPUnit/TextUI/TestRunner.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+if (isset($GLOBALS['monolog_on']) && $GLOBALS['monolog_on']) {
+    $log = new \Monolog\Logger('Log');
+    $log->pushHandler(new \Monolog\Handler\StreamHandler($GLOBALS['monolog_log']));
+}
+
 // Register services with the GuzzleTestCase
 Guzzle\Tests\GuzzleTestCase::setMockBasePath(__DIR__ . DIRECTORY_SEPARATOR . 'mock');
 
 Guzzle\Tests\GuzzleTestCase::setServiceBuilder(Guzzle\Service\Builder\ServiceBuilder::factory(array(
         'client' => array(
-            'class' => 'HealthGraph\HealthGraphClient'
+            'class' => 'HealthGraph\HealthGraphClient',
+            'params' => array(
+                'logger' => $log
+            )
         )
 )));
